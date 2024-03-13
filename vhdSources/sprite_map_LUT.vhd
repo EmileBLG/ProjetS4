99a3e96bf5e2aea6c07263c4393e38f435ecc5ee
Philou-X8 <pdurettesds@gmail.com>
2024-03-13 17:43:14 -0400

    sprite LUT prototype

--- vhdSources/sprite_map_LUT.vhd
+++ vhdSources/sprite_map_LUT.vhd
@@ -32,14 +32,35 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity sprite_map_LUT is
---Port ( 
-
---);
+Port ( 
+    i_index : in std_logic_vector (11 downto 0);
+    i_sprite_picker : in std_logic_vector (4 downto 0);
+    o_tile_type : out std_logic_vector (5 downto 0)
+);
 end sprite_map_LUT;
 
 architecture Behavioral of sprite_map_LUT is
 
-
+    type LUT is array (natural range <>) of std_logic_vector (5 downto 0);
+    signal LUT_map : LUT(0 to 255) := (
+    "000000",
+    "000001",
+    "000010",
+    "000011",
+    "000100",
+    "000101",
+    "000110",
+    "000111",
+    others => "000000"
+    );
+    
+    signal s_LUT_index : integer range 0 to 255;
+    
 begin
+    
+    s_LUT_index <= to_integer(unsigned(i_index));
+    
+    o_tile_type <= LUT_map(s_LUT_index);
+
 
 end Behavioral;
