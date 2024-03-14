719b53c35f161c0b078aa7e07a0d5f4f632238b5
Etienne Bellerive-Blais <57814653+EtienneBBlais@users.noreply.github.com>
2024-03-14 16:44:26 -0400

    INIT
    ajout du code C

---
+++ C code/Projet Visual Studio test en Console/flappy/flappy.h
@@ -0,0 +1,11 @@
+#ifndef FLAPPY_H
+#define FLAPPY_H
+
+#define ROWS 36
+#define COLS 64
+#define GRILLE 1000
+#define NBTUYAUX 4
+
+void initializeMatrix(char matrix[ROWS][COLS]);
+
+#endif /* MATRIX_H */
