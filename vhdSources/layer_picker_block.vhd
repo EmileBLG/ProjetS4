36214edf3020a68156ec2d09cf1fa6ead38e3e4c
Etienne Bellerive-Blais <57814653+EtienneBBlais@users.noreply.github.com>
2024-03-17 13:05:44 -0400

    Ajout du layer_Picker
    ajout d'un seul layer picker pcq idk pk il y en a 4

--- vhdSources/layer_picker_block.vhd
+++ vhdSources/layer_picker_block.vhd
@@ -32,14 +32,33 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity layer_picker_block is
---Port ( 
-
---);
+ port (
+        ColorCodeA : in std_logic_vector(3 downto 0);
+        IsHiddenA : in std_logic_vector(0 downto 0);
+        ColorCodeB : in std_logic_vector(3 downto 0);
+        IsHiddenB : in std_logic_vector(0 downto 0);
+        ColorCode : out std_logic_vector(3 downto 0);
+        IsHidden : out std_logic_vector(0 downto 0)
+    );
 end layer_picker_block;
 
 architecture Behavioral of layer_picker_block is
 
 
 begin
+process(ColorCodeA, ColorCodeB, IsHiddenA, IsHiddenB)
+    begin
+        if IsHiddenA = "1" or IsHiddenB = "1" then
+            IsHidden <= "1";
+        else
+            IsHidden <= "0";
+        end if;
+
+        if IsHiddenA = "0" then
+            ColorCode <= ColorCodeA;
+        else
+            ColorCode <= ColorCodeB;
+        end if;
+    end process;
 
 end Behavioral;
