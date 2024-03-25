2f3d8b4bdfde3980c4b267d4206b360e0a277657
Philou-X8 <pdurettesds@gmail.com>
2024-03-25 10:28:28 -0400

    new file

---
+++ vhdSources/sprite_tile_LUT_actor.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/21/2024 02:09:41 PM
+-- Design Name: 
+-- Module Name: sprite_tile_LUT_actor - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sprite_tile_LUT_actor is
+--  Port ( );
+end sprite_tile_LUT_actor;
+
+architecture Behavioral of sprite_tile_LUT_actor is
+
+begin
+
+
+end Behavioral;
